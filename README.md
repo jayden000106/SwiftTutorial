299dd02a81a76d2555cb53caa58d6002efb2ca61
jayden000106 <cars0106@icloud.com>
2022-04-07 01:09:49 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SwiftTutorial
+ developer.apple에 있는 SwiftUI Tutorials 따라하기
---
+++ SwiftTutorial/SwiftTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTutorialApp.swift
+//  SwiftTutorial
+//
+//  Created by 정지혁 on 2022/04/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

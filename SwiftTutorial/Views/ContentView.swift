de4e60a661e79f6ff086bdcb6a8360f37c97c634
jayden000106 <cars0106@icloud.com>
2022-04-08 01:04:11 +0900

    Finished building lists in chapter 1
    Building Lists and Navigation

---
+++ SwiftTutorial/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftTutorial
+//
+//  Created by 정지혁 on 2022/04/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
